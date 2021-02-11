cf4738fe461b8e31738bebe89ac84964c6633cb5
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-02-11 14:08:31 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,43 @@ import Team from '@sections/Team';
 import Faq from '@sections/Faq'; ';*/
 import Footer from '@sections/Footer';
 
+import {Row, Col, Container, Form, Button} from "react-bootstrap"
+
 const IndexPage = () => (
   <Layout>
     <Navbar />
     <Header />
     <About />
+    <Container>
+      <Form
+        name= "contact v1"
+        method="post"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <Row>
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>First Name </Form.Label>
+              <Form.Control required size="lg" type="text"/>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group>
+              <Form.Label>Last Name </Form.Label>
+              <Form.Control required size="lg" type="text"/>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group>
+              <Form.Label>How can we help? </Form.Label>
+              <Form.Control required as="textarea" rows="3" placeholder="What do you do?"/>
+        </Form.Group>
+        <Button type="submit"> Submit this </Button>
+      </Form>
+    </Container>
+        
+                
     <Footer />
   </Layout>
 );
