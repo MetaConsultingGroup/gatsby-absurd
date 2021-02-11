f1aad203a39bcd1cfae71403c0949bb122149d97
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-02-11 15:04:42 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import Team from '@sections/Team';
 import Faq from '@sections/Faq'; ';*/
 import Footer from '@sections/Footer';
 
-import {Row, Col, Container, Form, Button} from "react" 
+/*import {Row, Col, Container, Form, Button} from "react-bootstrap" */
 
 const IndexPage = () => (
   <Layout>
@@ -18,28 +18,28 @@ const IndexPage = () => (
     <Header />
     <About>
       <Container>
-        <Form name= "contact v1" method="post" data-netlify="true" onSubmit="submit">
+        <form name= "contact v1" method="post" data-netlify="true" onSubmit="submit">
          <input type="hidden" name="form-name" value="contact v1" />
           <Row>
             <Col>
-              <Form.Group>
+              <form.Group>
                 <Form.Label>First Name </Form.Label>
                 <Form.Control required size="lg" type="text"/>
-              </Form.Group>
+              </form.Group>
             </Col>
             <Col>
-              <Form.Group>
+              <form.Group>
                 <Form.Label>Last Name </Form.Label>
                 <Form.Control required size="lg" type="text"/>
-              </Form.Group>
+              </form.Group>
             </Col>
           </Row>
-          <Form.Group>
+          <form.Group>
                 <Form.Label>How can we help? </Form.Label>
                 <Form.Control required as="textarea" rows="3" placeholder="What do you do?"/>
-          </Form.Group>
+          </form.Group>
           <Button type="submit"> Submit </Button>
-        </Form>
+        </form>
       </Container>
     </About>
                 
