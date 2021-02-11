e5c92bd55c2113d5b4dae62aad843121df5bc51a
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-02-11 10:25:23 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Layout from '@common/Layout';
 import Navbar from '@common/Navbar';
 
 import Header from '@sections/Header';
-import About from '@sections/About'; /*
+import About from '@sections/About'; 
 import Brands from '@sections/Brands';
 import Team from '@sections/Team';
-import Faq from '@sections/Faq'; */
+import Faq from '@sections/Faq'; 
 import Footer from '@sections/Footer';
 
 const IndexPage = () => (
