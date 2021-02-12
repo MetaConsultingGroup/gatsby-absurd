1ce1e652c1d8e8191382f3697fe1abe30ad4f58d
MetaConsultingGroup <77215416+MetaConsultingGroup@users.noreply.github.com>
2021-02-12 13:30:05 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,33 +16,7 @@ const IndexPage = () => (
   <Layout>
     <Navbar />
     <Header />
-    <About>
-      /*<Container>
-        <form name= "contact v1" method="post" data-netlify="true" onSubmit="submit">
-         <input type="hidden" name="form-name" value="contact v1" />
-          <Row>
-            <Col>
-              <form.Group>
-                <Form.Label>First Name </Form.Label>
-                <Form.Control required size="lg" type="text"/>
-              </form.Group>
-            </Col>
-            <Col>
-              <form.Group>
-                <Form.Label>Last Name </Form.Label>
-                <Form.Control required size="lg" type="text"/>
-              </form.Group>
-            </Col>
-          </Row>
-          <form.Group>
-                <Form.Label>How can we help? </Form.Label>
-                <Form.Control required as="textarea" rows="3" placeholder="What do you do?"/>
-          </form.Group>
-          <Button type="submit"> Submit </Button>
-        </form>
-      </Container> */
-    </About>
-                
+    <About />               
     <Footer />
   </Layout>
 );
